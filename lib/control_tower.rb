836bb54b1058ce316f8f0803de095f2e88450ffe
joshua.ballanco@apple.com <joshua.ballanco@apple.com@23306eb0-4c56-4727-a40e-e92c0eb68959>
2010-05-11 02:06:36 +0000

    Vendoring rack for now.
    git-svn-id: http://svn.macosforge.org/repository/ruby/ControlTower/trunk@4065 23306eb0-4c56-4727-a40e-e92c0eb68959

--- lib/control_tower.rb
+++ lib/control_tower.rb
@@ -1,10 +1,10 @@
 # This file is covered by the Ruby license. See COPYING for more details.
 # Copyright (C) 2009-2010, Apple Inc. All rights reserved.
 
-require 'rubygems'
-require 'rack'
 require 'socket'
 require 'tempfile'
+$: << File.join(File.dirname(__FILE__), 'control_tower', 'vendor')
+require 'rack'
 require File.join(File.dirname(__FILE__), 'control_tower', 'rack_socket')
 require File.join(File.dirname(__FILE__), 'control_tower', 'server')
 
